15d39d42b08936c98e5b83ca9cb144cadb1ab318
c0d3t3k <73602268+c0d3t3k@users.noreply.github.com>
2020-12-07 17:41:37 -0500

    WIP Optics

--- src/optics.ts
+++ src/optics.ts
@@ -1,10 +1,31 @@
-import { subscribe } from "valtio";
+import { subscribe, proxy } from "valtio";
 import O from "optics-ts";
 
 export const useProxyFocus = <TBlur extends object, TFocus>(
   proxyObject: TBlur,
   optic: O.OpticFor<TBlur>
 ): TFocus => {
+  const getValueUsingOptic = <S, A>(
+    focus:
+      | O.Lens<S, any, A>
+      | O.Equivalence<S, any, A>
+      | O.Iso<S, any, A>
+      | O.Prism<S, any, A>
+      | O.Traversal<S, any, A>,
+    bigValue: S
+  ) => {
+    if (focus._tag === "Traversal") {
+      const values = O.collect(focus)(bigValue);
+      return values;
+    }
+    if (focus._tag === "Prism") {
+      const value = O.preview(focus)(bigValue);
+      return value;
+    }
+    const value = O.get(focus)(bigValue);
+    return value;
+  };
+
   let prevValue = proxyObject[key];
   return subscribe(proxyObject, () => {
     const nextValue = proxyObject[key];
