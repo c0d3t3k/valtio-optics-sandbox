52445db1074a27a877ed0e52a9084e41c5bc2de1
c0d3t3k <c0d3t3k@gmail.com>
2020-12-08 20:19:34 -0500

    WIP Valtio Optics

--- src/optics.ts
+++ src/optics.ts
@@ -3,6 +3,17 @@ import O from "optics-ts";
 import produce from "immer";
 import { useRef } from "react";
 
+const SENTINEL = {};
+
+// From https://github.com/facebook/react/issues/14490#issuecomment-451924162
+export function useRefFn<T>(init: () => T) {
+    const ref = useRef<T | typeof SENTINEL>(SENTINEL);
+    if (ref.current === SENTINEL) {
+        ref.current = init();
+    }
+    return ref as React.MutableRefObject<T>;
+}
+
 export type AnyOptic<S, A> =
   | O.Lens<S, any, A>
   | O.Equivalence<S, any, A>
@@ -31,16 +42,40 @@ const getValueUsingOptic = <S, A>(
   return value;
 };
 
+// From https://stackoverflow.com/a/31538091
+const isPrimitive = (val: any) => {
+  return (val !== Object(val));
+}
+
+export type MaybeSetter<T> = T | ((val: T) => T);
+export type MaybeOptic<T> = O.OpticFor<T> | (() => O.OpticFor<T>);
+
+export type InnerProxyWrapper<T> = { value: T }
+
 export const useProxyFocus = <TBlur extends object, TFocus extends object>(
   proxyObject: TBlur,
   optic: O.OpticFor<TBlur>
 ): TFocus => {
+  const initializeValue = (value: TFocus) => ({ value } as InnerProxyWrapper<TFocus>)
+
   const getter = (val: TBlur): TFocus =>
     (produce((val) => getValueUsingOptic<TBlur, TFocus>(optic as any, val))(
       val
     ) as unknown) as TFocus;
 
-  const innerProxyRef = useRef(proxy<TFocus>(getter(proxyObject)));
+  const setter = (val: TBlur, setVal: TFocus) => {
+    const isArray = Array.isArray(innerProxyRef);
+
+    const blurredNewVal = 
+
+    if(isArray) {
+      va
+    }
+  }
+
+  const innerProxyRef = useRefFn(() => proxy<InnerProxyWrapper<TFocus>>(initializeValue(getter(proxyObject))));
+
+  
 
   let prevValue = proxyObject[key];
   return subscribe(proxyObject, () => {
